3b3dd5fb840728f53ab33271b4bf93011980c86b
Denis <you@example.com>
2020-05-15 20:20:17 +0300

    Add to BSTU.FileCabinet.DAL interface IUnitOfWork.

---
+++ BSTU.FileCabinet/BSTU.FileCabinet.DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using BSTU.FileCabinet.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSTU.FileCabinet.DAL.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<Authorization, string> Authorizations { get; }
+        IRepository<Faculty, string> Faculties { get; }
+        IRepository<Group, int> Groups { get; }
+        IRepository<Pulpit, string> Pulpits { get; }
+        IRepository<Specialty, string> Specialties { get; }
+        IRepository<Student, int> Students { get; }
+        IRepository<Subject, string> Subjects { get; }
+        IRepository<Teacher, string> Teachers { get; }
+        IRepository<TeacherSubject, string> TeacherSubjects { get; }
+
+    }
+}
