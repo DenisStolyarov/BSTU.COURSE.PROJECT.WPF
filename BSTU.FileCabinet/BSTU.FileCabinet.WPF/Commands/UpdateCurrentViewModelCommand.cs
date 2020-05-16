f4fb519abebf3e59ab0339ab344f3739ccb3b9c5
Denis <you@example.com>
2020-05-16 13:41:27 +0300

    Add to BSTU.FileCabinet.WPF commands and implement INavigator.

---
+++ BSTU.FileCabinet/BSTU.FileCabinet.WPF/Commands/UpdateCurrentViewModelCommand.cs
@@ -0,0 +1,41 @@
+﻿using BSTU.FileCabinet.WPF.State.Navigators;
+using BSTU.FileCabinet.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BSTU.FileCabinet.WPF.Commands
+{
+    public class UpdateCurrentViewModelCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private readonly INavigator navigator;
+
+        public UpdateCurrentViewModelCommand(INavigator navigator)
+        {
+            this.navigator = navigator;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            var a = (ViewType)parameter;
+            switch (a)
+            {
+                case ViewType.Authorization:
+                    navigator.CurrentViewModel = new AuthorizationViewModel();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
--- BSTU.FileCabinet/BSTU.FileCabinet.WPF/State/Navigators/Navigator.cs
+++ BSTU.FileCabinet/BSTU.FileCabinet.WPF/State/Navigators/Navigator.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using BSTU.FileCabinet.WPF.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using PropertyChanged;
+using System.Windows.Input;
 
 namespace BSTU.FileCabinet.WPF.State.Navigators
 {
-    public class Navigator
+    [AddINotifyPropertyChangedInterface]
+    public class Navigator : INavigator
     {
+        public BaseViewModel CurrentViewModel { get; set; }
+
+        public ICommand UpdateCurrentViewModelCommand => throw new NotImplementedException();
     }
 }
